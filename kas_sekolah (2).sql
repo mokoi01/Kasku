dca9a3d0051baa07fe76bc2ab9766bfd3300292f
kenn <kenzoferdinan1@gmail.com>
2026-04-05 10:23:29 +0700

    Add files via upload

---
+++ kas_sekolah (2).sql
@@ -0,0 +1,389 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 30, 2026 at 09:57 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kas_sekolah`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TambahKasBaru` (IN `p_nis` INT, IN `p_id_kelas` INT, IN `p_nominal` INT, IN `p_id_status` INT)   BEGIN
+    INSERT INTO kas (nis, id_kelas, nominal, tanggal, id_status)
+    VALUES (p_nis, p_id_kelas, p_nominal, CURDATE(), p_id_status);
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `CekTotalSetoran` (`p_nis` INT) RETURNS INT DETERMINISTIC BEGIN
+    DECLARE total INT;
+    SELECT SUM(nominal) INTO total FROM kas WHERE nis = p_nis;
+    RETURN IFNULL(total, 0);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bendahara`
+--
+
+CREATE TABLE `bendahara` (
+  `id_bendahara` int NOT NULL,
+  `nis` int NOT NULL,
+  `nama_bendahara` varchar(255) NOT NULL,
+  `password_bendahara` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `id_kelas` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `bendahara`
+--
+
+INSERT INTO `bendahara` (`id_bendahara`, `nis`, `nama_bendahara`, `password_bendahara`, `email`, `id_kelas`) VALUES
+(1, 123, 'azka', 'azka123', 'azka@gmail.com', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kas`
+--
+
+CREATE TABLE `kas` (
+  `id_kas` int NOT NULL,
+  `nis` int NOT NULL,
+  `id_kelas` int NOT NULL,
+  `nominal` int NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kas`
+--
+
+INSERT INTO `kas` (`id_kas`, `nis`, `id_kelas`, `nominal`, `tanggal`, `id_status`) VALUES
+(1, 123456, 2, 3000, '2026-03-30', 1),
+(6, 123456, 1, 5000, '2026-03-30', 1);
+
+--
+-- Triggers `kas`
+--
+DELIMITER $$
+CREATE TRIGGER `BatasiNominalKas` BEFORE INSERT ON `kas` FOR EACH ROW BEGIN
+    IF NEW.nominal < 1000 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Waduh! Nominal kas minimal harus Rp 1.000 dong.';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int NOT NULL,
+  `nama_kelas` varchar(255) NOT NULL,
+  `pin_kelas` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `pin_kelas`) VALUES
+(1, 'RPL 1', 1),
+(2, 'RPL 2', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id_pengeluaran` int NOT NULL,
+  `id_kelas` int NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `nominal` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id_pengeluaran`, `id_kelas`, `deskripsi`, `nominal`) VALUES
+(1, 1, 'Hotel', 100000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sisa`
+--
+
+CREATE TABLE `sisa` (
+  `id_sisa` int NOT NULL,
+  `id_kelas` int NOT NULL,
+  `id_kas` int NOT NULL,
+  `id_pengeluaran` int NOT NULL,
+  `sisa` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `sisa`
+--
+
+INSERT INTO `sisa` (`id_sisa`, `id_kelas`, `id_kas`, `id_pengeluaran`, `sisa`) VALUES
+(1, 1, 1, 1, 20000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int NOT NULL,
+  `nis` int NOT NULL,
+  `nama_siswa` varchar(255) NOT NULL,
+  `id_kelas` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nis`, `nama_siswa`, `id_kelas`) VALUES
+(1, 123456, 'basuki', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id_status` int NOT NULL,
+  `deskripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id_status`, `deskripsi`) VALUES
+(1, 'BAYAR'),
+(2, 'BELUM BAYAR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wali_kelas`
+--
+
+CREATE TABLE `wali_kelas` (
+  `id_wali_kelas` int NOT NULL,
+  `nip` int NOT NULL,
+  `nama_wali_kelas` varchar(255) NOT NULL,
+  `password_wali_kelas` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `id_kelas` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `wali_kelas`
+--
+
+INSERT INTO `wali_kelas` (`id_wali_kelas`, `nip`, `nama_wali_kelas`, `password_wali_kelas`, `email`, `id_kelas`) VALUES
+(1, 123, 'Mis Ai', 'Mis Ai 123', 'MisAi@gmail.com', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bendahara`
+--
+ALTER TABLE `bendahara`
+  ADD PRIMARY KEY (`id_bendahara`),
+  ADD UNIQUE KEY `nis` (`nis`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `kas`
+--
+ALTER TABLE `kas`
+  ADD PRIMARY KEY (`id_kas`),
+  ADD KEY `nis` (`nis`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_status` (`id_status`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD UNIQUE KEY `pin_kelas` (`pin_kelas`);
+
+--
+-- Indexes for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `sisa`
+--
+ALTER TABLE `sisa`
+  ADD PRIMARY KEY (`id_sisa`),
+  ADD KEY `id_kas` (`id_kas`),
+  ADD KEY `id_pengeluaran` (`id_pengeluaran`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`),
+  ADD UNIQUE KEY `nis` (`nis`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- Indexes for table `wali_kelas`
+--
+ALTER TABLE `wali_kelas`
+  ADD PRIMARY KEY (`id_wali_kelas`),
+  ADD UNIQUE KEY `nip` (`nip`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bendahara`
+--
+ALTER TABLE `bendahara`
+  MODIFY `id_bendahara` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kas`
+--
+ALTER TABLE `kas`
+  MODIFY `id_kas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id_pengeluaran` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `sisa`
+--
+ALTER TABLE `sisa`
+  MODIFY `id_sisa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id_siswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id_status` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `wali_kelas`
+--
+ALTER TABLE `wali_kelas`
+  MODIFY `id_wali_kelas` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bendahara`
+--
+ALTER TABLE `bendahara`
+  ADD CONSTRAINT `bendahara_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `kas`
+--
+ALTER TABLE `kas`
+  ADD CONSTRAINT `kas_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `kas_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `kas_ibfk_3` FOREIGN KEY (`id_status`) REFERENCES `status` (`id_status`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD CONSTRAINT `pengeluaran_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `sisa`
+--
+ALTER TABLE `sisa`
+  ADD CONSTRAINT `sisa_ibfk_1` FOREIGN KEY (`id_kas`) REFERENCES `kas` (`id_kas`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `sisa_ibfk_2` FOREIGN KEY (`id_pengeluaran`) REFERENCES `pengeluaran` (`id_pengeluaran`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `sisa_ibfk_3` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `wali_kelas`
+--
+ALTER TABLE `wali_kelas`
+  ADD CONSTRAINT `wali_kelas_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
